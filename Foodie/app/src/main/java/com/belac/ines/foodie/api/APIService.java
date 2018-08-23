72956e40a684e7422e5994af91187a8a98a2040e
ibelac <ibelac@foi.hr>
2018-08-23 22:10:47 +0200

    Register Update
    Register aktivnost prilagođena korištenju novih biblioteka (Retrofit i Butter Knife)

--- Foodie/app/src/main/java/com/belac/ines/foodie/api/APIService.java
+++ Foodie/app/src/main/java/com/belac/ines/foodie/api/APIService.java
@@ -1,5 +1,6 @@
 package com.belac.ines.foodie.api;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
@@ -8,5 +9,10 @@ import retrofit2.http.POST;
 public interface APIService {
     @POST("/foodie/Login.php") @FormUrlEncoded
     Call<com.belac.ines.foodie.api.models.LoginResponse> login(@Field("email") String email, @Field("password") String password);
+
+    @POST("/foodie/Register.php") @FormUrlEncoded Call<ResponseBody> register(
+            @Field("name") String name, @Field("surname") String surname, @Field("email") String email,
+            @Field("telephone") String telephone, @Field("address") String address, @Field("restoran") String restoran,
+            @Field("password") String password);
 
 }
