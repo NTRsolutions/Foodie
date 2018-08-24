13433d2827b05add1c74e2cd78ab8c2714913ac0
ibelac <ibelac@foi.hr>
2018-08-24 10:48:26 +0200

    Update Home
    HomeActivity prilagođen korištenju novih biblioteka (Retrofit i Butter Knife).
    Izbrisan prikaz restorana na google maps.

--- Foodie/app/src/main/java/com/belac/ines/foodie/fragments/HomeFragment.java
+++ Foodie/app/src/main/java/com/belac/ines/foodie/fragments/HomeFragment.java
@@ -1,73 +1,53 @@
 package com.belac.ines.foodie.fragments;
 
 import android.Manifest;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.belac.ines.foodie.R;
-import com.belac.ines.foodie.api.AppConfig;
-import com.belac.ines.foodie.classes.Restoran;
-import com.belac.ines.foodie.helper.RestoranAdapter;
+import com.belac.ines.foodie.api.APIService;
+import com.belac.ines.foodie.api.RestaurantResponse;
+import com.belac.ines.foodie.api.RetrofitClient;
+import com.belac.ines.foodie.helper.HomeAdapter;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class HomeFragment extends Fragment {
 
-    public static final int CONNECTION_TIMEOUT=10000;
-    public static final int READ_TIMEOUT=15000;
+public class HomeFragment extends Fragment implements HomeAdapter.HomeListener {
 
-    MapView mMapView;
-    GoogleMap googleMap;
-    private List<Restoran> restoranList = new ArrayList<>();
-    private RecyclerView recyclerView;
-    private RestoranAdapter restoranAdapter;
-
+    private GoogleMap googleMap;
+    @BindView(R.id.recycler_view) RecyclerView recyclerView;
+    @BindView(R.id.progress_bar)  ProgressBar mProgressBar;
+    private SupportMapFragment mapFragment;
     private FusedLocationProviderClient mFusedLocationClient;
+    private final LatLng mDefaultLocation = new LatLng(51.509865, -0.118092);
+
+    private static final String TAG = HomeFragment.class.getSimpleName();
 
     public HomeFragment() {}
 
@@ -81,212 +61,69 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        mMapView= (MapView) view.findViewById(R.id.map_view);
-        mMapView.onCreate(savedInstanceState);
-        mMapView.onResume();
+        ButterKnife.bind(this, view);
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
-
-
-        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
-        restoranAdapter = new RestoranAdapter(restoranList, getContext());
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
-        recyclerView.setAdapter(restoranAdapter);
 
-        new AsyncTop3().execute("Porto");
-
-        try {
-            MapsInitializer.initialize(getActivity().getApplicationContext());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        mMapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap mMap) {
-                googleMap = mMap;
-                if (checkLocationPermission()) {
-                    if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                        mMap.setMyLocationEnabled(true);
-                    }
-                }
-
-                mMap.getUiSettings().setCompassEnabled(true);
-                mMap.getUiSettings().setMyLocationButtonEnabled(true);
-                mMap.getUiSettings().setRotateGesturesEnabled(true);
-            }
-        });
+        getData("Porto");
 
         return view;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        mMapView.onResume();
-    }
+    private void getData(String place) {
+        mProgressBar.setVisibility(View.VISIBLE);
+        RetrofitClient.instance()
+                .create(APIService.class)
+                .getTop3(place)
+                .enqueue(new Callback<List<RestaurantResponse>>() {
+                    @Override public void onResponse(@NonNull Call<List<RestaurantResponse>> call, @NonNull Response<List<RestaurantResponse>> response) {
+                        Log.d(TAG, "onResponse");
+                        mProgressBar.setVisibility(View.GONE);
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        mMapView.onPause();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mMapView.onDestroy();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mMapView.onLowMemory();
-    }
-
-    public boolean checkLocationPermission() {
-        if (ContextCompat.checkSelfPermission(getActivity(),
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                new AlertDialog.Builder(getActivity())
-                        .setTitle("")
-                        .setMessage("")
-                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                ActivityCompat.requestPermissions(getActivity(),new String[]
-                                        {Manifest.permission.ACCESS_FINE_LOCATION},1);
-                            }
-                        })
-                        .create().show();
-            } else {
-                ActivityCompat.requestPermissions(getActivity(),
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
-            } return false;
-        } else { return true; }
-    }
-
-    private class AsyncTop3 extends AsyncTask<String, String, String> {
-        ProgressDialog pdLoading = new ProgressDialog(getActivity());
-        HttpURLConnection conn;
-        URL url = null;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pdLoading.setMessage("\tLoading...");
-            pdLoading.setCancelable(false);
-            pdLoading.show();
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                url = new URL(AppConfig.URL_TOP3);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-                return "exception";
-            }
-            try {
-                // Setup HttpURLConnection class
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(READ_TIMEOUT);
-                conn.setConnectTimeout(CONNECTION_TIMEOUT);
-                conn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-
-                // Append parameters to URL
-                Uri.Builder builder = new Uri.Builder()
-                        .appendQueryParameter("from",params[0]);
-                String query = builder.build().getEncodedQuery();
-
-                // Open connection for sending data
-                OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-                writer.write(query);
-                writer.flush();
-                writer.close();
-                os.close();
-                conn.connect();
-
-            } catch (IOException e1) {
-                e1.printStackTrace();
-                return "exception";
-            }
-            try {
-                int response_code = conn.getResponseCode();
-                if (response_code == HttpURLConnection.HTTP_OK) {
-                    // Read data sent from server
-                    InputStream input = conn.getInputStream();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-                    StringBuilder result = new StringBuilder();
-                    String line;
-
-                    while ((line = reader.readLine()) != null) {
-                        result.append(line);
-                    }
-                    // Pass data to onPostExecute method
-                    return (result.toString());
-
-                } else { return ("unsuccessful"); }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                return "exception";
-            } finally {
-                conn.disconnect();
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-
-            try {
-                JSONArray json = new JSONArray(result);
-                int i;
-                for(i=0; i < json.length(); i++) {
-                    JSONObject jObject = json.getJSONObject(i);
-                    Restoran res = new Restoran(jObject.getString("name"),
-                            jObject.getString("address"),
-                            jObject.getDouble("longitude"),
-                            jObject.getDouble("latitude"),
-                            jObject.getDouble("distance"));
-                    restoranList.add(res);
-                }
-                restoranAdapter.notifyDataSetChanged();
-
-                if(restoranList.size() != 0){
-                    LatLngBounds.Builder builder = new LatLngBounds.Builder();
-                    LatLng marker = new LatLng(0,0);
-                    for(i = 0; i<restoranList.size(); i++) {
-                        marker = new LatLng(Float.parseFloat(String.valueOf(restoranList.get(i).getLatitude())),
-                                Float.parseFloat(String.valueOf(restoranList.get(i).getLongitude())));
-                        googleMap.addMarker(new MarkerOptions().position(marker).
-                                title(restoranList.get(i).getName()).snippet("distance:" + String.valueOf(restoranList.get(i).getDistance()*0.001)+ "km"));
-                        builder.include(marker);
+                        showData(response);
                     }
 
-                    LatLngBounds bounds = builder.build();
-                    int width = getResources().getDisplayMetrics().widthPixels;
-                    CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, width,300,50);
-                    googleMap.animateCamera(cu);
-                }
+                    @Override
+                    public void onFailure(@NonNull Call<List<RestaurantResponse>> call, @NonNull Throwable t) {
+                        Log.d(TAG, "onResponse");
+                        mProgressBar.setVisibility(View.GONE);
+                        Toast.makeText(getContext(), "Something went wrong.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 
-                if (result.equalsIgnoreCase("exception") || result.equalsIgnoreCase("unsuccessful")) {
-                    Toast.makeText(getActivity(), "OOPs! Something went wrong. Connection Problem.", Toast.LENGTH_LONG).show();
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-                Toast.makeText(getActivity(), "JSON problem: " + e.getMessage(), Toast.LENGTH_LONG).show();
-            }
-            pdLoading.dismiss();
-        }
+    private void showData(@NonNull Response<List<RestaurantResponse>> response) {
+
+        recyclerView.setAdapter(new HomeAdapter(response.body(), getContext(), this));
+    }
+
+    private void checkLocationPermission() {
+        Dexter.withActivity(getActivity())
+                .withPermissions(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        Log.d(TAG, "onPermissionsChecked");
+                    }
+
+                    @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions,
+                                                                             PermissionToken token) {
+                        Log.d(TAG, "onPermissionRationaleShouldBeShown");
+                        token.continuePermissionRequest();
+                    }
+                })
+                .check();
+    }
+
+    @Override
+    public void onClickLocation(LatLng latLng) {
+
+    }
+
+    @Override
+    public void onClickItem(RestaurantResponse item) {
+
     }
 }
